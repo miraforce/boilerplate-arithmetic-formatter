6a859d43754a8b3852892fa0f7822977318f3b64
mira <106882380+miraforce@users.noreply.github.com>
2023-07-19 16:18:40 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# This entrypoint file to be used in development. Start by reading README.md
+
+
+from arithmetic_arranger import arithmetic_arranger
+
+
+print(arithmetic_arranger(["738 + 78", "12 - 5534", "5 + 43", "123 + 49"]))
+
